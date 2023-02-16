a148f356db0b3fa36a83055f9578993338c3c560
Raphael Oliveira <raphaelbrf@gmail.com>
2023-02-15 23:31:04 -0300

    .

--- update_branch_main.sh
+++ update_branch_main.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo 'add and commit all code'
 git add .; git commit -m '.'; git push; git status
 
+echo 'update branch main'
 git checkout main; git merge dev; git push; git checkout dev
