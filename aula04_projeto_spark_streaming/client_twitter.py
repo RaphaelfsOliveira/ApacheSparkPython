669f4228720f030623da9ffa5a5e6ff2340b9fdc
Raphael Oliveira <raphaelbrf@gmail.com>
2023-02-22 15:38:11 -0300

    .

--- aula04_projeto_spark_streaming/client_twitter.py
+++ aula04_projeto_spark_streaming/client_twitter.py
@@ -1,16 +1,16 @@
 from pyspark.sql import SparkSession
 
-spark = SparkSession.builder.appName("SparkStreaming").getOrCreate()
+spark = SparkSession.builder.appName('SparkStreaming').getOrCreate()
 
 lines = spark.readStream\
-    .format("socket")\
-    .option("host", "localhost")\
-    .option("port", 9009) \
+    .format('socket')\
+    .option('host', 'localhost')\
+    .option('port', 9009) \
     .load()
 
 query = lines.writeStream \
-    .outputMode("append") \
-    .format("console") \
+    .outputMode('append') \
+    .format('console') \
     .start()
 
 query.awaitTermination()
--- aula04_projeto_spark_streaming/client_wordcount.py
+++ aula04_projeto_spark_streaming/client_wordcount.py
@@ -1,25 +1,25 @@
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as f
 
-spark = SparkSession.builder.appName("SparkStreaming").getOrCreate()
+spark = SparkSession.builder.appName('SparkStreaming').getOrCreate()
 
 lines = spark.readStream\
-    .format("socket")\
-    .option("host", "localhost")\
-    .option("port", 9009) \
+    .format('socket')\
+    .option('host', 'localhost')\
+    .option('port', 9009) \
     .load()
 
 words = lines.select(
     f.explode(
-        f.split(lines.value, " ")
-    ).alias("word")
+        f.split(lines.value, ' ')
+    ).alias('word')
 )
 
-wordCounts = words.groupBy("word").count()
+wordCounts = words.groupBy('word').count()
 
 query = wordCounts.writeStream \
-    .outputMode("complete") \
-    .format("console") \
+    .outputMode('complete') \
+    .format('console') \
     .start()
 
 query.awaitTermination()
