b10facb660b2acbd88b1d15d2ea2d7b555e7afe0
Raphael Oliveira <raphaelbrf@gmail.com>
2023-02-22 14:39:51 -0300

    .

--- aula02_projeto_spark_streaming/client.py
+++ aula02_projeto_spark_streaming/client.py
@@ -10,9 +10,5 @@ s.connect((HOST, PORT))
 while True:
     data = s.recv(1024)
     print(data.decode("utf-8"))
-    
-    # if len(data.decode("utf-8")) == 0: 
-    #     print(f'Fechando conexão client')
-    #     s.close()
 
-    time.sleep(2)
+    # time.sleep(2)
