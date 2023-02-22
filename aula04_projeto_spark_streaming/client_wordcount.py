e29262c2388936d1a9ad25bda994bd75b90db233
Raphael Oliveira <raphaelbrf@gmail.com>
2023-02-22 18:30:33 -0300

    .

--- aula04_projeto_spark_streaming/client_wordcount.py
+++ aula04_projeto_spark_streaming/client_wordcount.py
@@ -1,7 +1,10 @@
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as f
 
-spark = SparkSession.builder.appName('SparkStreaming').getOrCreate()
+spark = SparkSession.builder\
+    .config('spark.driver.host', '127.0.0.1')\
+    .appName('SparkStreaming')\
+    .getOrCreate()
 
 lines = spark.readStream\
     .format('socket')\
