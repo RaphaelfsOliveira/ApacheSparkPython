4d6a7b12a23708d04029d81dad76f4c5a32733fb
Raphael Oliveira <raphaelbrf@gmail.com>
2023-03-22 22:56:31 -0300

    .

--- aula03_projeto_spark_streaming/listener_twitter.py
+++ aula03_projeto_spark_streaming/listener_twitter.py
@@ -3,6 +3,9 @@ import tweepy
 
 HOST = 'localhost'
 PORT = 9009
+KEYWORD = 'economia politica'
+
+
 s = socket.socket()
 s.bind((HOST, PORT))
 print(f"Aguardando conexão na porta: {PORT}")
@@ -12,7 +15,6 @@ connection, address = s.accept()
 print(f"Recebendo solicitação de {address}")
 
 BEARER_TOKEN = 'AAAAAAAAAAAAAAAAAAAAAEWIUAEAAAAAU49qkkegQd58kO0B%2FIB77iclVLg%3DUFuFnFZiMhocJdBIZQ1n7ygcqE8l4CeXvpMuzjZPziMWY5jhaE'
-keyword = 'politica'
 
 class GetTweets(tweepy.StreamingClient):
 
@@ -22,7 +24,7 @@ class GetTweets(tweepy.StreamingClient):
         connection.send(tweet.text.encode('utf-8', 'ignore'))
 
 client = GetTweets(BEARER_TOKEN)
-client.add_rules(tweepy.StreamRule(keyword))
+client.add_rules(tweepy.StreamRule(KEYWORD))
 client.filter()
 
 
